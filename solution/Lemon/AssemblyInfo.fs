acb2df4903a6c70ee281414c66733a2dd5e0ae10
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-11-11 17:11:25 +0700

    NuGetパッケージ構成の見直し

---
+++ solution/Lemon/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Lemon2")>]
+[<assembly: AssemblyDescription("This library supports functional programming using F# on ASP.NET.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Lemon2")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("83425D7B-F5EE-4D74-86B2-D02C1CA566AA")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+()
